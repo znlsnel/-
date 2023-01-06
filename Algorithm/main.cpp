2933c114f0b3594ab460d12dfc536c7a1609c638
Udi <dnel0605@gmail.com>
2023-01-06 11:36:56 +0900

    위상 정렬

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,28 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
+#define MAX 7
 
-int n = 1000;
-bool a[1000];
+vector<int> v[MAX];
+int inDegree[MAX + 1];
 
-void PrimeNumber() 
+void TopologySort()
 {
-	for (int i = 2; i < n; i++)
-		a[i] = true;
 
-	for (int i = 2; i < n; i++)
+	queue<int> q;
+	vector<int> result;
+
+	for (int i = 1; i <= MAX ; i++)
+		if (inDegree[i] == 0) q.push(i);
+
+	for (int i = 1; i <= MAX; i++)
 	{
-		if (a[i] == false) continue;
-		for (int j = i * 2; j < n; j += i)
-			a[j] = false;
+		if (q.empty())
+		{
+			cout << "순환 발생" << endl;
+			return;
+		}
+		
+		int a = q.front();
+		result.push_back(a);
+		q.pop();
+
+		for (int j = 0; j < v[a].size(); j++)
+		{
+			int b = v[a][j];
+			if (--inDegree[b] == 0)
+			{
+				q.push(b);
+			}
+		}
 	}
-	for (int i = 2; i < n; i++)
-		if (a[i]) cout << i << " ";
+	for (int i = 0; i < result.size(); i++)
+		cout << result[i] << " ";
+	
 }
 
 int main()
 {
-	PrimeNumber();
+
+	v[1].push_back(2);
+	inDegree[2]++;
+	v[1].push_back(5);
+	inDegree[5]++;
+	v[2].push_back(3);
+	inDegree[3]++;
+	v[3].push_back(4);
+	inDegree[4]++;
+	v[4].push_back(6);
+	inDegree[6]++;
+	v[5].push_back(6);
+	inDegree[6]++;
+	v[6].push_back(7);
+	inDegree[7]++;
+	TopologySort();
 	return 0;
 }
-
